a809613156b12ce8646280c8cb233fd94f709898
Migue Wan <104940106+miguewan@users.noreply.github.com>
2024-12-12 17:08:44 -0300

    Imagen en base64 y logg
    - Imagen en base64
    - Log de carga

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,22 +8,36 @@ import * as Tesseract from "tesseract.js";
 })
 export class AppComponent {
   title = "ocr";
+  progressLabel: string = '';
+  progressNr: number = 0;
 
   extractedText: string = "";
+  constructor() {}
 
-  async performOCR(imageFile: File) {
-    const result = await Tesseract.recognize(
-      imageFile,
-      'eng',
-      { logger: m => console.log(m) }
-    );
-    this.extractedText = result.data.text;
+  async performOCR(imageFile: string) {
+    const result = await Tesseract.recognize(imageFile, "eng", { logger: (m) => {console.log('m',m)
+      this.progressLabel = m.status;
+      this.progressNr = Math.round(m.progress *100);
+    } });
+    this.extractedText = result.data.text; //.replace(/[^\w]/gi, '');
+    console.log("Finalizado: ", result);
   }
 
   onFileSelected(event: any) {
     if (event.target.files && event.target.files.length > 0) {
-      const imageFile = event.target.files[0];
-      this.performOCR(imageFile);
+      // const imageFile = event.target.files[0];
+      // this.performOCR(imageFile);
+
+      const reader = new FileReader();
+
+      reader.readAsDataURL(event.target.files[0]);
+      let base64Image: any;
+
+      reader.onload = (event: any) => {
+        base64Image = event.target.result;
+        console.log("base64Image", base64Image);
+        this.performOCR(base64Image);
+      };
     }
   }
 }
